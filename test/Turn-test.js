ac5cb6a02bc91c3e76996f5dab4b47850fce2024
Nick Hart <nick.hart.des@gmail.com>
2020-06-29 17:01:24 -0600

    Add all necessary methods to Turn class to pass testing suite

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -16,57 +16,57 @@ describe('Turn', function() {
         expect(turn).to.be.an.instanceOf(Turn);
     });
 
-    it.skip('should include an answer', function() {
+    it('should include an answer', function() {
         const turn = new Turn('Answer');    
         expect(turn.answer).to.equal('Answer');
     });
 
-    it.skip('should include an instance of Card', function() {
+    it('should include an instance of Card', function() {
         const card = new Card();
         const turn = new Turn('Answer', card);
 
         expect(turn.card).to.equal(card);
     });
 
-    it.skip('should be able to return a guess', function() {
+    it('should be able to return a guess', function() {
         const card = new Card();
         const turn = new Turn('Answer', card);
 
         expect(turn.returnGuess()).to.equal('Answer');
     });
 
-    it.skip('should be able to return the card', function() {
+    it('should be able to return the card', function() {
         const card = new Card();
         const turn = new Turn('Answer', card);
 
         expect(turn.returnCard()).to.equal(card);
     });
 
-    it.skip('should be able to tell you if you get the answer wrong', function() {
+    it('should be able to tell you if you get the answer wrong', function() {
         const card = new Card(1, 'What is Nick\'s cat\'s name?', ['Toby', 'Hobbes', 'Ollie'], 'Hobbes');
         const turn = new Turn('Toby', card);
 
         expect(turn.evaluateGuess()).to.equal(false);
     });
 
-    it.skip('should be able to tell you if you get the answer right', function() {
+    it('should be able to tell you if you get the answer right', function() {
         const card = new Card(1, 'What is Nick\'s cat\'s name?', ['Toby', 'Hobbes', 'Ollie'], 'Hobbes');
         const turn = new Turn('Hobbes', card);
         
         expect(turn.evaluateGuess()).to.equal(true);
     });
 
-    it.skip('should be able to return the correct response if your guess is wrong', function () {
+    it('should be able to return the correct response if your guess is wrong', function () {
         const card = new Card(1, 'What is Nick\'s cat\'s name?', ['Toby', 'Hobbes', 'Ollie'], 'Hobbes');
         const turn = new Turn('Toby', card);
 
         expect(turn.giveFeedback()).to.equal('incorrect!');
     });
 
-    it.skip('should be able to return the correct response if your guess is right', function() {
+    it('should be able to return the correct response if your guess is right', function() {
         const card = new Card(1, 'What is Nick\'s cat\'s name?', ['Toby', 'Hobbes', 'Ollie'], 'Hobbes');
         const turn = new Turn('Hobbes', card);
 
         expect(turn.giveFeedback()).to.equal('correct!');
-    })
+    });
 });
