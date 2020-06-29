15302c576338b51a00af9d18a96decda4a22514c
Nick Hart <nick.hart.des@gmail.com>
2020-06-29 15:53:04 -0600

    Add test for false evaluateGuess() response

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -41,4 +41,11 @@ describe('Turn', function() {
 
         expect(turn.returnCard()).to.equal(card);
     });
+
+    it.skip('should be able to tell you if you get the answer wrong', function() {
+        const card = new Card(1, 'What is Nick\'s cat\'s name?', ['Toby', 'Hobbes', 'Ollie'], 'Hobbes');
+        const turn = new Turn('Toby', card);
+
+        expect(turn.evaluateGuess()).to.equal(false);
+    });
 });
