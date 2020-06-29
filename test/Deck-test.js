9a85663967fe2eda5c4e714c4dcef4190be079bc
Nick Hart <nick.hart.des@gmail.com>
2020-06-29 17:12:18 -0600

    Add first two tests looking for constructor function

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,5 +6,14 @@ const Card = require('../src/Card');
 
 describe('Deck', function() {
 
+    it('should be a function', function() {
+        const deck = new Deck();
+        expect(Deck).to.be.a('function');
+    });
+
+    it('should be an instance of Deck', function() {
+        const deck = new Deck();
+        expect(deck).to.be.an.instanceOf(Deck);
+    });
 
 });
