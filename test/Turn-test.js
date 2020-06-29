25bbed5e274dd2ef3a9265dd7fb775b79a64a6a7
Nick Hart <nick.hart.des@gmail.com>
2020-06-29 15:18:25 -0600

    Create first test looking for constructor

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,3 +2,12 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+    it.skip('should be a function', function() {
+        const turn = new Turn();
+        expect(Turn).to.be.a('function');
+    });
+
+});
