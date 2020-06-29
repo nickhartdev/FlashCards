433008d07ce41e253a02d4460c5af566856ccc27
Nick Hart <nick.hart.des@gmail.com>
2020-06-29 15:32:50 -0600

    Create skeleton of Turn class, passing first two tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,19 +2,24 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
 
 describe('Turn', function() {
 
-    it.skip('should be a function', function() {
+    it('should be a function', function() {
         const turn = new Turn();
         expect(Turn).to.be.a('function');
     });
 
-    it.skip('should be an instance of Turn', function() {
+    it('should be an instance of Turn', function() {
         const turn = new Turn();
         expect(turn).to.be.an.instanceOf(Turn);
     });
 
+    // it.skip('should be able to return an answer', function() {
+    //     const card = new Card(1, 'What is Nick\'s cat\'s name?', ['Toby', 'Hobbes', 'Ollie'], 'Hobbes');
+    //     const turn = new Turn('Hobbes', card);
 
-
+    //     expect(turn.answer).to.deep.equal(card.answers[2]);
+    // });
 });
