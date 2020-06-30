dab80bae2e3cfb2cf1a35fa2c85f1f8099b88f07
Nick Hart <nick.hart.des@gmail.com>
2020-06-29 19:42:34 -0600

    Add arguments to card instantiations and takeTurn invocations

--- test/Round-test.js
+++ test/Round-test.js
@@ -8,9 +8,9 @@ const Turn = require('../src/Turn');
 
 describe('Round', function() {
     beforeEach(function() {
-        const card1 = new Card();
-        const card2 = new Card();
-        const card3 = new Card();
+        const card1 = new Card(1, 'What is the name of Nick\'s cat?', ['Toby', 'Hobbes', 'Ollie'], 'Hobbes');
+        const card2 = new Card(2, 'Where is Ili originally from?', ['Denver', 'San Antonio', 'Austin'], 'San Antonio');
+        const card3 = new Card(3, 'Which drum corps did Nick march with?', ['Blue Devils', 'Blue Stars', 'Blue Knights'], 'Blue Knights');
         const deck = new Deck([card1, card2, card3]);
         const round = new Round(deck);
     });
@@ -30,7 +30,7 @@ describe('Round', function() {
     it.skip('should update the turn count every time a guess is made', function() {
         expect(round.turns).to.equal(0);
         
-        round.takeTurn();
+        round.takeTurn('Hobbes');
 
         expect(round.turns).to.equal(1);
     });
@@ -38,17 +38,21 @@ describe('Round', function() {
     it.skip('should move to the next card after a guess is made', function() {
         expect(round.returnCurrentCard()).to.equal(card1);
 
-        round.takeTurn();
+        round.takeTurn('Hobbes');
 
         expect(round.returnCurrentCard()).to.equal(card2);
     });
 
     it.skip('should have a way of keeping track of incorrect guesses', function() {
-
+        expect(round.incorrectGuesses).to.exist;
     });
 
     it.skip('should update the number of incorrect guesses when you guess wrong', function() {
+        expect(round.incorrectGuesses).to.deeply.equal(0);
 
+        round.takeTurn('Toby');
+
+        expect(round.incorrectGuesses).to.deeply.equal(1);
     });
 
     it.skip('should let you know when you get a question right', function() {
