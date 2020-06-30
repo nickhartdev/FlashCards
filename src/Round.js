22c3bed5224bde7ccc428ba55780385ba49e55af
Nick Hart <nick.hart.des@gmail.com>
2020-06-29 20:35:50 -0600

    Add takeTurn()

--- src/Round.js
+++ src/Round.js
@@ -1,10 +1,15 @@
 class Round {
     constructor(deck) {
         this.deck = deck;
+        this.turns = 0;
     }
 
     returnCurrentCard() {
         return this.deck.cards[0];
+    }
+
+    takeTurn() {
+        this.turns++;
     }
 }
 
