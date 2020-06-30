487e6765ef7440d75f3173953ef305af0f47f923
Nick Hart <nick.hart.des@gmail.com>
2020-06-29 18:46:13 -0600

    Add tests for Round constructor

--- test/Round-test.js
+++ test/Round-test.js
@@ -7,5 +7,13 @@ const Card = require('../src/Card');
 
 describe('Round', function() {
 
-    
+    it('should be a function', function() {
+        const round = new Round();
+        expect(Round).to.be.a('function');
+    });
+
+    it('should be an instance of Round', function() {
+        const round = new Round();
+        expect(round).to.be.an.instanceOf('Round');
+    });
 });
