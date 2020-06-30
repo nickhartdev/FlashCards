432430ad9f2c8ec33dbc6c02a1e41721f08a3f3b
Nick Hart <nick.hart.des@gmail.com>
2020-06-30 15:49:25 -0600

    Call new game instance locally for each test instead of globally

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,21 +1,23 @@
 const chai = require('Chai');
 const expect = chai.expect;
 
-const game = require('../src/Game');
+const Game = require('../src/Game');
 
 describe('Game', function() {
 
-    const game = new Game();
-
     it.skip('should be a function', function() {
+        const game = new Game();
         expect(Game).to.be.a('function');
     });
 
     it.skip('should be an instance of Game', function() {
+        const game = new Game();
         expect(game).to.be.an.instanceOf(Game);
     });
 
     it.skip('should keep track of the current round', function() {
+        const game = new Game();
+
         expect(game.currentRound).to.equal(0);
 
         game.round.takeTurn();
@@ -24,12 +26,13 @@ describe('Game', function() {
         expect(game.currentRound).toEqual(2);
     });
 
-    it('should start the game', function() {
+    it.skip('should start the game', function() {
+        const game = new Game();
         expect(game.start()).to.exist();
     });
 
     it('should have cards', function() {
-        
+        const Game = new Game();
     });
 
     it('should keep the cards in a deck', function() {
