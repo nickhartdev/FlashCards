82c7b5f6270d81c90d10182469340b30cbd0827d
Nick Hart <nick.hart.des@gmail.com>
2020-06-30 15:37:41 -0600

    Create outline for testing suite

---
+++ test/Game-test.js
@@ -0,0 +1,43 @@
+const chai = require('Chai');
+const expect = chai.expect;
+
+const game = require('../src/Game');
+
+describe('Game', function() {
+
+    it('should be a function', function() {
+
+    });
+
+    it('should be an instance of Game', function() {
+
+    });
+
+    it('should keep track of the current round', function() {
+
+    });
+
+    it('should start the game', function() {
+
+    });
+
+    it('should have cards', function() {
+
+    });
+
+    it('should keep the cards in a deck', function() {
+
+    });
+
+    it('should be able to start a new round', function() {
+
+    });
+
+    it('should invoke printMessage', function() {
+
+    });
+
+    it('should invoke printQuestion', function() {
+
+    });
+})
