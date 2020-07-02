ce3d42ea871c09193ed2e23ee24c27a50ea73610
Nick Hart <nick.hart.des@gmail.com>
2020-07-02 12:57:06 -0600

    Get rid of test 3

--- test/Game-test.js
+++ test/Game-test.js
@@ -9,18 +9,12 @@ const Round = require('../src/Round');
 describe('Game', function() {
 
     it('should be a function', function() {
-        const game = new Game();
         expect(Game).to.be.a('function');
     });
 
     it('should be an instance of Game', function() {
         const game = new Game();
         expect(game).to.be.an.instanceOf(Game);
-    });
-
-    it('should have a way of keeping track of the current round', function() {
-        const game = new Game();
-        expect(game.currentRound).to.exist;
     });
 
     it('should have a way to start', function() {
