5b8278b0734ea0a9d3ab4a075de4f7737d89abae
Nick Hart <nick.hart.des@gmail.com>
2020-07-02 13:04:17 -0600

    Resolve linter warnings

--- test/Game-test.js
+++ test/Game-test.js
@@ -8,54 +8,52 @@ const Round = require('../src/Round');
 
 describe('Game', function() {
 
-    it('should be a function', function() {
-        expect(Game).to.be.a('function');
-    });
+  it('should be a function', function() {
+    expect(Game).to.be.a('function');
+  });
 
-    it('should be an instance of Game', function() {
-        const game = new Game();
-        expect(game).to.be.an.instanceOf(Game);
-    });
+  it('should be an instance of Game', function() {
+    const game = new Game();
+    expect(game).to.be.an.instanceOf(Game);
+  });
 
-    it('should have a way to start', function() {
-        const game = new Game();
-        expect(game.start).to.be.a('function');
-    });
+  it('should have a way to start', function() {
+    const game = new Game();
+    expect(game.start).to.be.a('function');
+  });
 
-    it('should create an array of cards when it starts', function() {
-        const game = new Game();
+  it('should create an array of cards when it starts', function() {
+    const game = new Game();
 
-        expect(game.generateCards()).to.be.an('array');
-        expect(game.generateCards()[0]).to.be.an.instanceOf(Card);
-    });
+    expect(game.generateCards()).to.be.an('array');
+    expect(game.generateCards()[0]).to.be.an.instanceOf(Card);
+  });
 
-    it('should store the array of cards in a deck when it starts', function() {
-        const game = new Game();
-        const cards = game.generateCards();
+  it('should store the array of cards in a deck when it starts', function() {
+    const game = new Game();
+    const cards = game.generateCards();
 
-        expect(game.createDeck(cards)).to.be.an('object');
-        expect(game.createDeck(cards)).to.be.an.instanceOf(Deck);
-    });
+    expect(game.createDeck(cards)).to.be.an('object');
+    expect(game.createDeck(cards)).to.be.an.instanceOf(Deck);
+  });
 
-    it('should create a new Round instance using the Deck', function() {
-        const game = new Game();
-        const cards = game.generateCards();
-        const deck = game.createDeck(cards);
+  it('should create a new Round instance using the Deck', function() {
+    const game = new Game();
+    const cards = game.generateCards();
+    const deck = game.createDeck(cards);
 
-        expect(game.startNewRound(deck)).to.be.an('object');
-        expect(game.startNewRound(deck)).to.be.an.instanceOf(Round);
-    });
+    expect(game.startNewRound(deck)).to.be.an('object');
+    expect(game.startNewRound(deck)).to.be.an.instanceOf(Round);
+  });
 
-    it('should have a currentRound property that returns the current round', function() {
-        // By running this test, it will actually start the game in order to
-        // check the current round. Feel free to exit the process.
+  it('should have a currentRound property that returns the current round', function() {
+    // By running this test, it will actually start the game in order to
+    // check the current round. Feel free to exit the process.
 
-        const game = new Game();
-        const cards = game.generateCards();
-        const deck = game.createDeck(cards);
+    const game = new Game();
 
-        game.start();
+    game.start();
 
-        expect(game.currentRound).is.an.instanceOf(Round);
-    });
+    expect(game.currentRound).is.an.instanceOf(Round);
+  });
 })
